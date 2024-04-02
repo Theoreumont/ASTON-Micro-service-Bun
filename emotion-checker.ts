1fa9b1cc0906469290b687188e089b16ff976c62
Théo Reumont <theoreumontpro@gmail.com>
2024-04-02 13:47:19 +0200

    fix: add try & catch

--- emotion-checker.ts
+++ emotion-checker.ts
@@ -2,23 +2,28 @@ import {env} from "./env"
 import openai from "openai"
 
 export async function detectEmotion(input: string) {
-	const OpenAI = new openai({
-		apiKey: env.OPEN_AI_API_KEY
-	})
-	const completion = await OpenAI.chat.completions.create({
-		messages: [
-			{
-				role: "system",
-				content:
-					"You are a helpful assistant designed to return the sentiment of a given text. The JSON object format is {sentiment: 'positive' | 'negative' | 'neutral'}."
-			},
-			{role: "user", content: input}
-		],
-		model: "gpt-3.5-turbo-0125",
-		response_format: {type: "json_object"}
-	})
+	try {
+		const OpenAI = new openai({
+			apiKey: env.OPENAI_API_KEY
+		})
+		const completion = await OpenAI.chat.completions.create({
+			messages: [
+				{
+					role: "system",
+					content: `You are a helpful assistant designed to return the sentiment of a given text. 
+						The JSON object format is {sentiment: 'positive' | 'negative' | 'neutral'}.`
+				},
+				{role: "user", content: input}
+			],
+			model: "gpt-3.5-turbo-0125",
+			response_format: {type: "json_object"}
+		})
 
-	const sentiment = completion.choices[0].message.content
+		const sentiment = completion.choices[0].message.content
 
-	return sentiment
+		return sentiment
+	} catch (err) {
+		console.error(err)
+		return null
+	}
 }
