e758ff0d74ce8185490e79ee6ac343b92e4e22c1
Théo Reumont <theoreumontpro@gmail.com>
2024-04-02 13:47:40 +0200

    fix: add a check to verify if it is a open ai key

--- env.ts
+++ env.ts
@@ -1,11 +1,17 @@
 import z from "zod"
 
 export const envSchema = z.object({
-	OPEN_AI_API_KEY: z.string({
-		required_error: "Set OPEN_AI_API_KEY in .env file"
-	})
+	OPENAI_API_KEY: z
+		.string({
+			required_error: "Set OPENAI_API_KEY in .env file"
+		})
+		.startsWith("sk-", {
+			message: "OPENAI_API_KEY must start with 'sk-', are you sure this is an OpenAI API key?"
+		})
 })
 
 export type Env = z.infer<typeof envSchema>
 
 export const env = envSchema.parse(process.env)
+
+console.log("env", env)
